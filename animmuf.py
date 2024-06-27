7fe9ea41138cca53c4dbf39d31d0d59ae040de42
Fred C <github-fred@hidzz.com>
2024-06-26 18:48:24 -0700

    Fix some idiosyncrasies.

--- animmuf.py
+++ animmuf.py
@@ -5,6 +5,7 @@ import gc
 import json
 import logging
 import os
+import pathlib
 import sys
 import time
 from subprocess import PIPE, Popen
@@ -23,16 +24,17 @@ logging.basicConfig(
 )
 logger = logging.getLogger('animmuf')
 
+
 def read_config():
-  home = os.path.expanduser('~')
+  home = pathlib.Path('~').expanduser()
   config_path = (
-    os.path.join('.', CONFIG_NAME),
-    os.path.join(home, '.' + CONFIG_NAME),
-    os.path.join(home, '.local', CONFIG_NAME),
-    os.path.join('/etc', CONFIG_NAME),
+    pathlib.Path('.').joinpath(CONFIG_NAME),
+    pathlib.Path(home).joinpath('.' + CONFIG_NAME),
+    pathlib.Path(home).joinpath('.local', CONFIG_NAME),
+    pathlib.Path('/etc').joinpath(CONFIG_NAME),
   )
   for filename in config_path:
-    if os.path.exists(filename):
+    if filename.exists():
       break
     logger.debug('Config file "%s" not found', filename)
   else:
@@ -40,27 +42,28 @@ def read_config():
     sys.exit(os.EX_CONFIG)
 
   logger.debug('Reading config file "%s"', filename)
-  with open(filename, 'r', encoding='utf-8') as confd:
+  with filename.open('r', encoding='utf-8') as confd:
     config = yaml.safe_load(confd)
   return type('Config', (object,), config)
 
 
 def retrieve_files(config):
+  muf_file = pathlib.Path(config.muf_file)
   try:
-    file_time = os.stat(config.muf_file).st_mtime
+    file_time = muf_file.stat().st_mtime
     if time.time() - file_time < 3600:
       return
   except FileNotFoundError:
     pass
 
-  urlretrieve(SOURCE_JSON, config.muf_file)
-  logger.info('Downloading: %s, into: %s', SOURCE_JSON, config.muf_file)
-  with open(config.muf_file, 'r', encoding='utf-8') as fdin:
+  logger.info('Downloading: %s, into: %s', SOURCE_JSON, muf_file)
+  urlretrieve(SOURCE_JSON, muf_file)
+  with muf_file.open('r', encoding='utf-8') as fdin:
     data_source = json.load(fdin)
     for url in data_source:
-      filename = os.path.basename(url['url'])
-      target_name = os.path.join(config.target_dir, filename)
-      if os.path.exists(target_name):
+      filename = pathlib.Path(url['url'])
+      target_name = pathlib.Path(config.target_dir).joinpath(filename.name)
+      if target_name.exists():
         continue
       urlretrieve(NOAA + url['url'], target_name)
       logger.info('%s saved', target_name)
@@ -69,43 +72,41 @@ def retrieve_files(config):
 def cleanup(config):
   """Cleanup old muf image that are not present in the json manifest"""
   logger.info('Cleaning up non active MUF images')
+  muf_file = pathlib.Path(config.muf_file)
   current_files = set([])
-  with open(config.muf_file, 'r', encoding='utf-8') as fdm:
+  with muf_file.open('r', encoding='utf-8') as fdm:
     data = json.load(fdm)
     for entry in data:
-      current_files.add(os.path.basename(entry['url']))
+      current_files.add(pathlib.Path(entry['url']).name)
 
-  for name in os.listdir(config.target_dir):
-    if name.startswith('CTIPe-MUF') and name not in current_files:
+  target_dir = pathlib.Path(config.target_dir)
+  for filename in target_dir.glob('CTIPe-MUF_*'):
+    if filename.name not in current_files:
       try:
-        os.unlink(os.path.join(config.target_dir, name))
-        logger.info('Delete file: %s', name)
+        filename.unlink()
+        logger.info('Delete file: %s', filename)
       except IOError as exp:
         logger.error(exp)
 
 
 def animate(config):
+  target_dir = pathlib.Path(config.target_dir)
   try:
-    resampling = Image.Resampling.LANCZOS # This if for new versions of PIL
+    resampling = Image.Resampling.LANCZOS  # This if for new versions of PIL
   except AttributeError:
     resampling = Image.LANCZOS            # Older versions of PIL
 
   # suitables image size (1290, 700) (640, 400) (800, 600)
   img_size = (800, 600)
   font = ImageFont.truetype(config.font, int(config.font_size))
-  animation = os.path.join(config.target_dir, 'muf.gif')
+  animation = target_dir.joinpath('muf.gif')
   image_list = []
 
-  file_list = []
-  for name in sorted(os.listdir(config.target_dir)):
-    if not name.startswith('CTIPe-MUF_'):
-      continue
-    file_list.append(name)
-
+  file_list = sorted(target_dir.glob('CTIPe-MUF_*'))
   logger.info('Processing: %d images', len(file_list))
-  for name in file_list:
-    logger.debug('Add %s', name)
-    image = Image.open(os.path.join(config.target_dir, name))
+  for filename in file_list:
+    logger.debug('Add %s', filename.name)
+    image = Image.open(filename)
     image = image.convert('RGB')
     image = image.resize(img_size, resampling)
     draw = ImageDraw.Draw(image)
@@ -124,15 +125,17 @@ def animate(config):
 
 
 def gen_video(config):
-  logfile = os.path.join(config.target_dir, 'muf.log')
-  gif_file = os.path.join(config.target_dir, 'muf.gif')
+  target_dir = pathlib.Path(config.target_dir)
+  logfile = target_dir.joinpath('muf.log')
+  gif_file = target_dir.joinpath('muf.gif')
 
-  if not os.path.isfile(config.converter):
+  converter = pathlib.Path(config.converter)
+  if not converter.exists():
     logger.error('Video converter %s not found', config.converter)
     return
 
-  cmd = f'{config.converter} {gif_file} {config.video_file}'
-  with open(logfile, "w", encoding='utf-8') as fdlog:
+  cmd = f'{converter} {gif_file} {config.video_file}'
+  with logfile.open("w", encoding='utf-8') as fdlog:
     print(cmd, file=fdlog)
     with Popen(cmd.split(), shell=False, stdout=PIPE, stderr=fdlog) as proc:
       logger.info("Saving %s video file", config.video_file)
